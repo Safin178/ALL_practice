3224a00bb3463bfe9b3f610012ec7f4c4320cc76
Afrin Hasan Safin <114832343+Safin178@users.noreply.github.com>
2023-02-10 17:01:14 +0000

    hi

---
+++ git.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    
+    
+    */
+    int a = 1819;
+    printf("%d\n", a);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}

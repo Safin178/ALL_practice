5a19d4763cff26eecea7ba9ebe79c24b97a55601
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-02-20 22:41:26 +0600

    new

---
+++ CF839A.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    
+    
+    */
+        int i, n,a,b;
+        char c;
+        scanf("%d", &n);
+        for(i = 1; i <= n ; i++)
+        {
+            scanf("%d%c%d", &a,&c,&b);
+
+            printf("%d\n", a+b);
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}

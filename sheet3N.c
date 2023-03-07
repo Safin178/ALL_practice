afb651a320a7c851d5a48f97b503d5d7aba3fefa
Afrin Hasan Safin <114832343+Safin178@users.noreply.github.com>
2023-03-07 06:33:24 +0000

    7/3

--- sheet3N.c
+++ sheet3N.c
@@ -6,14 +6,11 @@ int main()
     scanf("%d%d", &a, &b);
      char s[a+b+2];
     scanf("%s", s);
-    if(s[a] != '-')
-    {
-        f = 1;
-    }
+    
     for(int i = 0; i< (a+b+2);i++)
     {
-        if(i == a)
-        {continue;}
+        if(i == a && s[a] != '-')
+        {f = 1; continue;}
         if(s[i] > '9'&& s[i] < '0' )
         {
             f =1;

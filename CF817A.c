0b2d5999f89a485ac5c9577e04fd728fb3330d86
Afrin Hasan Safin <114832343+Safin178@users.noreply.github.com>
2023-03-07 09:22:08 +0000

    7\3

--- CF817A.c
+++ CF817A.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     //https://codeforces.com/contest/1722/problem/A
-   long long  int n, x,i, t, l;
+   long long  int n, x,i, t, l, j;
    scanf("%lld", &t);
    for(l = 0 ; l < t; l++)
    {
      scanf("%lld", &n);
     char s[n];
     scanf(" %s", s);
+    j = strlen(s);
     for(i = 0, x=0  ; i < n ; i++)
     {
         x += s[i];
     }
-    if(x == 529)
+    if(x == 529 && j == 5)
     {
         printf("YES\n");
     }

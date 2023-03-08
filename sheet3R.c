9206c816ad932090b1f29425e13a2b1233e75ccf
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-03-08 13:50:42 +0600

    no

---
+++ sheet3R.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    
+    rr
+
+    */
+    freopen("input.txt", "r+", stdin);
+    freopen("output.txt", "w+", stdout);
+    int n ;
+    scanf("%d", &n);
+
+    printf("%d", n+1);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}

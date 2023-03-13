3421ca826a08f1531d8c3656d54995bcbfac0131
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-03-13 23:56:49 +0600

    WA

--- 1722B.c
+++ 1722B.c
@@ -9,24 +9,26 @@ int main()
     scanf("%d", &t);
     for(int l = 0 ; l < t; l++)
     {
-        int n;
+        int n, x1= 0 , x2= 0;
         scanf("%d", &n);
         char a[n], b[n];
         scanf("%s %s", a, b);
         int f = 0;
         for(int i = 0; i < n; i++)
         {
-            if((a[i] == 'R') || (b[i] == 'R'))
-            {
-                if(a[i] != b[i])
+            if(a[i] != b[i])
                 {
-                    f=1;
-                    break;
+                    f = 1;
+                    
                 }
+            if(a[i] == 'R')
+            {
+                x1++;
             }
+            if(b[i] == 'R') {x2++;}
         }
-        if(f == 0) printf("YES\n");
-        else printf("NO\n");
+        if((x1 == x2)||(f == 0)){ printf("YES\n");}
+        else {printf("NO\n");}
     }
     
     

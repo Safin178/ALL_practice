c75f79a874e4c025512b45632d6290291bd24c7c
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-03-26 19:20:24 +0600

    WA

---
+++ sheet4/219856F.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    /*
+    
+    
+    */
+   int n , l;
+   
+   scanf("%d", &n);
+   for(l = 0 ; l < n ; l++ )
+   {
+    char str[105];
+      scanf(" %s ", str);
+      int x = strlen(str);
+      if(x < 10)
+      {
+        printf("%s\n", str);
+
+      }
+      else{
+        printf("%c%d%c\n", str[0] , x-2, str[x-1]);//x -1 bul hoccilo age x disilam
+      }
+   }
+
+   
+    
+    return 0;
+}

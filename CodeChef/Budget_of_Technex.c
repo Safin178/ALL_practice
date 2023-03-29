979a940fe06bd0d639dcee4e661b7797613b3d3c
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-03-29 22:08:24 +0600

    WA

---
+++ CodeChef/Budget_of_Technex.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    
+    
+    */
+    long long int t, a, b;
+    scanf("%lld", &t);
+    while(t--)
+    {
+        scanf("%lld", &a);
+        a  = a * 1000;
+        a  = (a / 2);
+        printf("%lld\n", a / 5);
+       
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}

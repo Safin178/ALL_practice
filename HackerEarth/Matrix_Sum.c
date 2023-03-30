09c2a18f50b212819aae8ac38dfdfd0f2a908a8c
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-03-30 18:02:43 +0600

    WA but 1st test case milse

---
+++ HackerEarth/Matrix_Sum.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    /*
+
+
+    */
+    long long n, m;
+    scanf("%lld%lld", &n, &m);
+    long long i, j;
+    long long a[n][m], b[n][m];
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%lld", &a[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%lld", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            
+            printf("%lld ", (a[i][j] + b[i][j]));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

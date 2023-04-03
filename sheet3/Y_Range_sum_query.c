46013d1246c0c0a5e944b838595dd565716320b6
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-04-03 17:08:49 +0600

    new approach yt er moto but long long er karone AC hoccilo na  ekn AC

--- sheet3/Y_Range_sum_query.c
+++ sheet3/Y_Range_sum_query.c
@@ -9,23 +9,31 @@ void solve(void)
     //loop diye korte partam but prefix sum practice...
 
     //https://youtu.be/7pJo_rM0z_s 
-    int n , m, i;
-    scanf("%d%d", &n,&m);
-    int arr[n+1];
-    int feq[n+1];
-    feq[0] = 0;
+    long long int n , m, i;
+    scanf("%lld%lld", &n,&m);
+    long long int arr[n+1];
+    long long int feq[n+1];
+    
     for(i = 1 ; i <= n; i++)
     {
-        scanf("%d", &arr[i]);
+        scanf("%lld", &arr[i]);
+    }
+    feq[1] = arr[1];
+    feq[0] = 0;
+    for(i = 2 ; i <= n; i++)
+    {
+        
         feq[i] = arr[i] + feq[i - 1];
 
     }
-    int x, y, ans;
+    
+
+    long long int x, y, ans;
     for(i = 1 ; i <= m ; i++)
     {
-        scanf("%d%d", &x, &y);
-        ans = feq[y] - feq[x-1];
-        printf("%d\n", ans);
+        scanf("%lld%lld", &x, &y);
+        ans = feq[y] - feq[x - 1];
+        printf("%lld\n", ans);
 
     }
 

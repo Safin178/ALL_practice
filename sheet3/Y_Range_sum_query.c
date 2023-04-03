05e577a99995f018e4931c874274fca1374fcf81
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-04-03 08:44:23 +0600

    pore

---
+++ sheet3/Y_Range_sum_query.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<limits.h>
+
+void solve(void)
+{
+    //loop diye korte partam but prefix sum practice...
+
+    //https://youtu.be/7pJo_rM0z_s 
+
+
+
+
+    
+}
+
+int main()
+{
+    int t = 1;
+    //scanf("%d", &t);   // remove '//' for test case
+    
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}

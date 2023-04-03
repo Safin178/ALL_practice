71631d4133d2b32e4114cfdeb86738323fe5e60a
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-04-03 16:44:57 +0600

    WA on test case 4 astese

--- sheet3/Y_Range_sum_query.c
+++ sheet3/Y_Range_sum_query.c
@@ -9,8 +9,25 @@ void solve(void)
     //loop diye korte partam but prefix sum practice...
 
     //https://youtu.be/7pJo_rM0z_s 
+    int n , m, i;
+    scanf("%d%d", &n,&m);
+    int arr[n+1];
+    int feq[n+1];
+    feq[0] = 0;
+    for(i = 1 ; i <= n; i++)
+    {
+        scanf("%d", &arr[i]);
+        feq[i] = arr[i] + feq[i - 1];
 
+    }
+    int x, y, ans;
+    for(i = 1 ; i <= m ; i++)
+    {
+        scanf("%d%d", &x, &y);
+        ans = feq[y] - feq[x-1];
+        printf("%d\n", ans);
 
+    }
 
 
     

e1b0d94d9a969f308c6f46f0d28bb4ec7fd2eac7
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-04-09 22:33:54 +0600

    loop beshi cilo tai AC hoccilo na , tle diccilo, kinto ekta loop er modde kore felate hoi gese

--- AtCoder/C_PC_on_the_Table.c
+++ AtCoder/C_PC_on_the_Table.c
@@ -10,38 +10,42 @@ void solve(void)
     scanf("%d%d", &n, &m);
     char s[105][105];
 
-     for(i = 1; i <= n; i++)
+     for(i = 0; i < n; i++)
     {
-        for(j = 1; j <= m;j++)
+        scanf(" %s ", &s[i]);
+        for(j = 0; j < m-1;j++)
         {
-            scanf(" %c ", &s[i][j]);
-            
-        }
-    }
-    for(i = 1; i <= n; i++)
-    {
-        for(j = 1; j <= m-1;j++)
-        {
-            if(s[i][j] == 'T' && s[i][j+1] == 'T')
+              if(s[i][j] == 'T' && s[i][j+1] == 'T')
             {
                 s[i][j] = 'P';
                 s[i][j+1]= 'C';
             }
-        }
-    }
-     for(i = 1; i <= n; i++)
-    {
-        for(j = 1; j <= m;j++)
-        {
-            printf("%c", s[i][j]);
             
         }
-        printf("\n");
+        printf("%s\n", s[i]);
+    }
+    // for(i = 1; i <= n; i++)
+    // {
+    //     for(j = 1; j <= m-1;j++)
+    //     {
+    //         if(s[i][j] == 'T' && s[i][j+1] == 'T')
+    //         {
+    //             s[i][j] = 'P';
+    //             s[i][j+1]= 'C';
+    //         }
+    //     }
+    // }
+    //  for(i = 1; i <= n; i++)
+    // {
+    //     for(j = 1; j <= m;j++)
+    //     {
+    //         printf("%c", s[i][j]);
+            
+    //     }
+    //     printf("\n");
             
         
-    }
- 
-
+    //
 
     
 }

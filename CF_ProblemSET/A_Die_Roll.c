65f01ae0c51f492b5ac4419108042c5192a66add
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-04-10 07:40:28 +0600

    WA

--- CF_ProblemSET/A_Die_Roll.c
+++ CF_ProblemSET/A_Die_Roll.c
@@ -1,40 +1,42 @@
 #include<stdio.h>
+
+int gcd( int x,int y)
+{
+    int rem;
+    
+    while(rem != 0)
+    {
+        rem = x % y;
+        x = y;
+        y = rem;
+    }
+    //euclide bhai er sotro hoito
+
+
+
+
+    return x;
+}
 int main()
 {
     /*
     
     
     */
-    int a , b, ans;
-    scanf("%d%d",&a, &b );
+    // int a , b, ans;
+    // scanf("%d%d",&a, &b );
 
-    if(a>= b)
-    {
-        ans = 6 - a +1;
-    }
-    else{
-        ans = 6 - b + 1;
-    }
-    if(ans == 0)
-    {
-        printf("0/1");
-        return 0;
-    }
-    printf("1/%d", 6 / ans);
+    // if(a>= b)
+    // {
+    //     ans = 6 - a +1;
+    // }
+    // else{
+    //     ans = 6 - b + 1;
+    // }
+    // printf("%d/%d", ans/gcd(ans,6), 6/gcd(ans,6));
+    int p = 6/gcd(5,6);
+    int d = gcd(6,5) ;
+    printf("%d/%d",d,p);
 
-    //mm
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     return 0;
 }

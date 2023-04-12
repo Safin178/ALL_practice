0e0ee1a07a54a7e52b905583af416c34a1723a78
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-04-12 16:21:39 +0600

    AC

---
+++ Random/PatternODD.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    https://telegra.ph/PatternODD-04-12
+    
+    */
+    int n, i, j, k; 
+    scanf("%d", &n);//n should be an odd number..
+
+     
+
+     char arr[n+1][n+1];//1 - index 
+
+     int x = (n / 2);
+     x++;
+     
+     
+
+     for(i = 1, k = n; i <= n; i++, k--)
+     {
+        for(j = 1 ; j <= n ; j++)
+        {
+            arr[i][j] = '*';
+            arr[i][i]= '\\';
+            arr[i][k] = '/';
+        }
+     }
+     arr[x][x] = 'x';
+
+     for(i = 1; i <= n; i++)
+     {
+        for(j = 1; j <= n ; j++)
+        {
+            
+            printf("%c", arr[i][j]);
+        }
+        printf("\n");
+     }
+    
+    return 0;
+}

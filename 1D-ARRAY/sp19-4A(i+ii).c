c6bf5ba7e49f700b50168a3e46bc5443d114ba6c
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-05 13:44:02 +0600

    new

---
+++ 1D-ARRAY/sp19-4A(i+ii).c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    
+    
+    */
+    //4>i
+    char direction[10] ="WEST";
+    direction[4] = '\0';
+    //sesh
+    //printf("%s", direction); eta just check korar jonno liksi, direction[4] = 0; dileo kaj korbe karon null er ascii 0
+    //direction[3] = '\0'; dile WES print hobe 
+    
+    //4 > ii
+    int arr[3][4] = {10,12,14,0,0,20,22,0,0,30,32,0};
+
+    //sesh  //1 ta row purota like tarporer row , tarporer row//
+    
+    /*for(int i = 0; i < 3;i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }*/
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}

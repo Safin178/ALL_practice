c60572b69ef5faabc256df65320b1139a1779225
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-05 23:34:38 +0600

    11:34

---
+++ Function-Recursion/sp19-2d.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+// similar like Aut22-2C
+int divisorcheck(int x, int y)
+{
+    if(x % y == 0 || y % x == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int r;
+    r = divisorcheck(a, b);
+    if( r == 1)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+    return 0;
+}

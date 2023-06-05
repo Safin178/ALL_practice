4fc2c86302c0d6dec1ea3342d90b8afccdb7bce8
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-05 21:50:42 +0600

    no

--- nested loop/sp221d.c
+++ nested loop/sp221d.c
@@ -14,7 +14,7 @@ int main()
     */
     int x, i = 0, sum = 0;
     double avg;
-    for(;;)//i++ eikane o dite partam
+    for(;;)//for eibabe na diye while(1) dite parbo (ar ekta jinish ami for(;1;) eirokom na likheo code colce,kemne jani na)
     {
         scanf("%d", &x);
         if(x < 0)
@@ -23,12 +23,12 @@ int main()
         }
         if(x % 3 == 0 || x % 5 == 0)
         {
-            continue;
+            continue;//i++ upore likle continue er jonno o i++ hoye jabe , so jegula continue hobe na oigular jonnoi i++ korbo,,
         }
         i++;
         sum += x;
     }
-    printf("%.2lf", sum / (double)i);
+    printf("%.2lf, %d, %d", sum / (double)i, sum, i);
     
     
     
---
+++ string+2D array/strcmp.c
@@ -0,0 +1,63 @@
+// #include<stdio.h>
+
+// this is incorrect ,,experimenting
+
+
+
+// #include<string.h>
+// int comp(char x[], char y[])
+// {
+//     int i = 0;
+//     char a[10], b[10];
+
+//     for(i = 0; x[i] != 0; i++)
+//     {
+//         a[i] = x[i];
+//         b[i] = y[i];
+//         if(a[i] >= 'A' && a[i] <= 'Z')
+//         {
+//             a[i]= a[i] +32;
+//         }
+//          if(b[i] >= 'A' && b[i] <= 'Z')
+//         {
+//             b[i]= b[i] +32;
+//         }
+//         
+//         if(a[i] > b[i])
+//         {
+//             return 1;
+//         }
+//         else if(a[i] < b[i])
+//         {
+           
+//             return -1;
+//         }
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     /*
+    
+    
+//     */
+//     char x[10] = "cd";
+//     char y[10] = "ABc";
+
+//     int l = comp(x, y);
+//     printf("%d", l);
+//     printf(" %d\n", strcmp(x,y));
+//     printf("%s %s", x, y);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     return 0;
+// }

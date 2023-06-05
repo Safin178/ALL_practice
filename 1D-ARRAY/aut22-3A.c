ba8b79d11cfa9af454ef45e791808f42cca773f4
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-05 13:18:42 +0600

    new

---
+++ 1D-ARRAY/aut22-3A.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    
+    
+    */
+    int arr[6] = {1,4,3,2,5,6};
+
+    int i;
+    for(i = 0; i < 6 ; i++)
+    {
+        printf("i = %d, A[%d] = %d\n", i, i,arr[i]);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
